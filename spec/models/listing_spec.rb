d7c2207950b0ad24a5f01ef34727c142bb2c1be2
Theosaurus-Rex <theosaurus13@gmail.com>
2021-02-18 10:47:11 +1100

    add ActiveRecord validation to Listing model, add relevant Rspec test

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -2,4 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Listing, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  it "should have a title" do
+    new_user = User.create!(
+      name: "Bob Marley",
+      email: "email@address.com",
+      password: "Password1"
+    )
+    invalid_listing = Listing.new(
+      description: "cool card",
+      price: 500,
+      sold: false,
+      user: new_user
+    )
+
+    expect(invalid_listing.valid?).to be(false)
+  end
 end
